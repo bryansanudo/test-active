117f87af5b294f10403122e8565ba7b8b5761ff8
Bryan <bryansanudo95@gmail.com>
2023-09-16 12:01:03 -0500

    do it

--- src/MiComponente.jsx
+++ src/MiComponente.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class MiComponente extends React.Component {
   handleClick = () => {
     const script = document.createElement("script");
-    script.src = "https://bryandofus3229.activehosted.com/f/embed.php?id=7";
+    script.src = "https://bryandofus3229.activehosted.com/f/embed.php?id=9";
     script.type = "text/javascript";
     script.charset = "utf-8";
     document.body.appendChild(script);
