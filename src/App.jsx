55fa61f8cf41cee7af18cd7076914b2e37944ca9
Bryan <bryansanudo95@gmail.com>
2023-09-16 11:20:46 -0500

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
+  const hola = () => {};
+
   return (
     <>
       <div>Mi Pagina Web para registrar en activeCampaing</div>
+      <button onClick={hola}>hello world</button>
     </>
   );
 }
